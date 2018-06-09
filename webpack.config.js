84bcfd6d750f7d8b9531cb7c609e36d48c5a14c3
maxuuell <maxuuell@gmail.com>
2018-06-09 13:43:30 -0700

    Add eslint, and fix lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const env = process.env.NODE_ENV;
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index-bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: env === 'production'
+					? ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: ['css-loader'],
+					})
+					: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
 };
